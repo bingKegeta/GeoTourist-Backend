daa33a7c61a804d8d90f954943ecee8d3f6d7970
Da-Emp66 <asherbean10@gmail.com>
2023-11-21 04:33:35 -0500

    dataset fabrication ready

--- location_suggest/dataset.py
+++ location_suggest/dataset.py
@@ -8,26 +8,9 @@ import random
 from geopy.exc import GeocoderTimedOut
 from geopy.geocoders import Nominatim
 
-# """
-# Important Note:
-# Ensure that when you run this file, you use the same user_id
-# for both set_class_data and the rest of the file. If you ran
-# set_class_data with a different user than you run the rest of
-# the file, the file will not be able to map classes correctly.
-
-# If you are not sure what this means, run with:
-#     `--set_class_data=True`
-# It will slow down the data generation process but only by
-# about three minutes.
-# """
-
-#TODO:
-# ClimateZone mappings
-# Four Location Sampling
-
 ONEHOT_MAPPING = {
-    'Trewartha' : ['Ar', 'Am', 'Aw', 'Cf', 'Cs', 'Cw', 'Cr', 'Do', 'Dc', 'Eo', 'Ec', 'Ft', 'Fi', 'BW', 'BS'], # Wet to dry
-    'ClimateZone' : [],
+    'Trewartha' : ['Ar', 'Am', 'Aw', 'Cf', 'Cs', 'Cw', 'Cr', 'Do', 'Dc', 'Eo', 'Ec', 'Ft', 'Fi', 'BW', 'BS'], # Wet to dry ~ Related by extremity of climate
+    'ClimateZone' : ['Subtropical Monsoon', 'Tropical Wet', 'Tropical Wet-And-Dry', 'Subtropical Humid', 'Subtropical Dry', 'Temperate Continental', 'Temperate Oceanic', 'Boreal, Continental Subarctic', 'Steppe or Semiarid', 'Tundra', 'Desert or Arid',], # Wet to dry ~ Related by extremity of climate
 }
 
 
@@ -35,13 +18,17 @@ def make_graphql_request(query_filename: str = "./gql/users.graphql", variables:
     """
     Send a Query or Mutate request to GraphQL and get a response.
     Sample uses:
-    >>>print(make_graphql_request().json()['data'])
-    >>>print(make_graphql_request("./gql/locations.graphql", {'user_id': USER_IDS}).json()['data'])
+    >>>print(make_graphql_request()['data'])
+    >>>print(make_graphql_request("./gql/locations.graphql", {'user_id': USER_IDS})['data'])
     """
     with open(query_filename) as gql_query:
         query = gql_query.read()
         gql_query.close()
-    return requests.post("http://localhost:5000/api", json={'query': query , 'variables': variables})
+    result = requests.post("http://localhost:5000/api", json={'query': query , 'variables': variables}).json()
+    for key in result['data']:
+        if result['data'][key] == "Failure!":
+            raise Exception("GraphQL query returned \"Failure!\"")
+    return result
 
 def get_coordinates_by_name(city: str, geolocator: Nominatim = Nominatim(user_agent="tourfusion"), tries: int = 5) -> Optional[Tuple[float, float]]:
     """Return latitude and longitude of the given city."""
@@ -64,30 +51,33 @@ def location_json_to_list(location_json: Dict[str, Any]) -> List[Any]:
 def add_random_locations(user_id: str, k: int) -> None:
     """Add random locations to the user with the given user_id's account."""
     for i in range(k):
-        print(make_graphql_request(
-                    "./gql/addLocations.graphql",
-                    {
-                        'user_id': user_id,
-                        'name': {
-                            'display': f"location_{i}",
-                            'street': f"street_{i}",
-                            'city': f"city_{i}",
-                            'country': f"country_{i}",
-                            'address': f"address_{i}",
-                            'postal': f"postal_{i}"
-                        },
-                        'latitude': random.random() * 180 - 90,
-                        'longitude': random.random() * 360 - 180
-                    }
-                ).json()
-            )
+        try:
+            result = make_graphql_request(
+                        "./gql/addLocations.graphql",
+                        {
+                            'user_id': user_id,
+                            'name': {
+                                'display': f"location_{i}",
+                                'street': f"street_{i}",
+                                'city': f"city_{i}",
+                                'country': f"country_{i}",
+                                'address': f"address_{i}",
+                                'postal': f"postal_{i}"
+                            },
+                            'latitude': random.random() * 180 - 90,
+                            'longitude': random.random() * 360 - 180
+                        }
+                    )
+            print(f"{i+1}/{k}: {result}")
+        except Exception as e:
+            print(e)
 
 def query_location_data(user_id: str) -> List[Dict[str, Any]]:
     """
     Grab all location data for all locations held by the user
     with the given user_id.
     """
-    return make_graphql_request("./gql/locations.graphql", {'user_id': user_id}).json()['data']['locations']
+    return make_graphql_request("./gql/locations.graphql", {'user_id': user_id})['data']['locations']
 
 def set_class_data(user_id: str, possible_locations: str) -> None:
     """
@@ -111,43 +101,29 @@ def set_class_data(user_id: str, possible_locations: str) -> None:
     # Get remaining features
     location_features = []
     for idx, row in df.iterrows():
-        make_graphql_request(
-                "./addLocations.graphql",
-                {
-                    'user_id': user_id,
-                    'name': {
-                        'display': f"location_{idx}",
-                        'street': f"street_{idx}",
-                        'city': row['City'],
-                        'country': row['Country'],
-                        'address': f"address_{idx}",
-                        'postal': f"postal_{idx}"
-                    },
-                    'latitude': row['Latitude'],
-                    'longitude': row['Longitude']
-                }
-            )
-        location_features.append(
-                location_json_to_list(
-                    query_location_data(user_id)
+        try:
+            make_graphql_request(
+                    "./gql/addLocations.graphql",
+                    {
+                        'user_id': user_id,
+                        'name': {
+                            'display': f"location_{idx}",
+                            'street': f"street_{idx}",
+                            'city': row['City'],
+                            'country': row['Country'],
+                            'address': f"address_{idx}",
+                            'postal': f"postal_{idx}"
+                        },
+                        'latitude': row['Latitude'],
+                        'longitude': row['Longitude']
+                    }
                 )
-            )
-        make_graphql_request(
-                "./deleteLocation.graphql",
-                {
-                    'user_id': user_id,
-                    'name': {
-                        'display': f"location_{idx}",
-                        'street': f"street_{idx}",
-                        'city': row['City'],
-                        'country': row['Country'],
-                        'address': f"address_{idx}",
-                        'postal': f"postal_{idx}"
-                    },
-                    'latitude': row['Latitude'],
-                    'longitude': row['Longitude']
-                }
-            )
+            added_location = make_graphql_request("./gql/locations.graphql", {'user_id': user_id})['data']['locations'][0]
+            location_features.append(location_json_to_list(added_location))
+            make_graphql_request("./gql/deleteLocation.graphql", { '_id': added_location['_id'] })
+            print(f"Row added successfully: {added_location}")
+        except Exception as e:
+            print(e)
 
     location_features = np.array(location_features)
 
@@ -158,35 +134,41 @@ def set_class_data(user_id: str, possible_locations: str) -> None:
     
     # Write the newfound data to the classes file
     df.to_csv(possible_locations, index=False)
+    print("Data successfully written to possible_locations csv!")
 
 def encode(df: pd.DataFrame, column: str) -> pd.DataFrame:
     words = df[column].to_list()
+    # print(f"ENCODE {column}: {words}")
     return pd.DataFrame({column : [ONEHOT_MAPPING[column].index(word) for word in words]})
 
 def decode(df: pd.DataFrame, column: str) -> pd.DataFrame:
     onehot_encoding = df[column].to_list()
-    return pd.DataFrame({column : [ONEHOT_MAPPING[column][onehot_number] for onehot_number in onehot_encoding]})
+    # print(f"DECODE {column}: {onehot_encoding}")
+    return pd.DataFrame({column : [ONEHOT_MAPPING[column][int(onehot_number)] for onehot_number in onehot_encoding]})
 
-def sample_and_agglomerate_locations(user_id: str, possible_locations: str) -> pd.DataFrame:
+def sample_and_agglomerate_locations(user_id: str, possible_locations: str, k: int = 100) -> pd.DataFrame:
     """
     Create a dataset using the possible_locations file for class information.
     """
 
+    print("Beginning dataset agglomeration.")
     # Get some random locations to work with
-    add_random_locations(user_id, 100)
+    add_random_locations(user_id, k)
     locations = query_location_data(user_id)
 
     # Remove all oceanic locations and force a 0 ranking
     locations =  np.array([[0] + location_json_to_list(location) for location in locations if location['elevation'] != 0])
-    locations[:,4] = encode(pd.DataFrame({'Trewartha' : locations[:,4]}), 'Trewartha').to_numpy()
-    locations[:,5] = encode(pd.DataFrame({'ClimateZone' : locations[:,5]}), 'ClimateZone').to_numpy()
+    assert len(locations) > 0
+    locations[:,5] = encode(pd.DataFrame({'Trewartha' : locations[:,5]}), 'Trewartha').to_numpy()[0]
+    locations[:,6] = encode(pd.DataFrame({'ClimateZone' : locations[:,6]}), 'ClimateZone').to_numpy()[0]
+    locations = np.array([[float(location) for location in location_set] for location_set in locations])
 
     # Read from the classes file
     data = pd.read_csv(possible_locations)
 
     # Grab classes as "{city}, {country}" and encode with one-hot
     classes = (data['City'] + ", " + data['Country']).to_list()
-    encoded_classes = (data['Rank'] - 1).to_numpy()
+    encoded_classes = (data['Rank'].astype(float) - 1.).to_numpy()
 
     # Map the encodings to their respective city names for later decoding
     ONEHOT_MAPPING['City, Country'] = {}
@@ -195,33 +177,40 @@ def sample_and_agglomerate_locations(user_id: str, possible_locations: str) -> p
 
     # Grab all features
     features = data[['Rank', 'Latitude', 'Longitude', 'Elevation', 'AverageTemperature', 'Trewartha', 'ClimateZone']]
-    encoded_features = (features[['Rank', 'Latitude', 'Longitude', 'Elevation', 'AverageTemperature']] + encode(features, column='Trewartha') + encode(features, column='ClimateZone')).to_numpy()
-
+    encoded_trewartha = encode(features, column='Trewartha')
+    encoded_climatezone = encode(features, column='ClimateZone')
+    encoded_features = np.c_[
+            features[['Rank', 'Latitude', 'Longitude', 'Elevation', 'AverageTemperature']].astype(float).to_numpy(),
+            encoded_trewartha['Trewartha'].astype(float).to_numpy(),
+            encoded_climatezone['ClimateZone'].astype(float).to_numpy()
+        ]
     # Get the closest label for every one of our random locations
     classifier = sklearn.neighbors.KNeighborsClassifier(n_neighbors=1).fit(encoded_features, encoded_classes)
     predictions = classifier.predict(locations)
 
     # Decode using the one-hot mapping made earlier
-    classifications = pd.DataFrame({'Class' : [ONEHOT_MAPPING['City, Country'][prediction] for prediction in predictions]})
+    classifications = pd.DataFrame({'Class' : [ONEHOT_MAPPING['City, Country'][int(prediction)] for prediction in predictions]})
 
-    return pd.DataFrame(
-        {
-            'Latitude' : locations[:,0],
-            'Longitude' : locations[:,1],
-            'Elevation' : locations[:,2],
-            'AverageTemperature' : locations[:,3],
-        }
-    ) + \
-    decode(pd.DataFrame({'Trewartha' : locations[:,4]}), 'Trewartha') + \
-    decode(pd.DataFrame({'ClimateZone' : locations[:,5]}), 'ClimateZone') + \
-    classifications
+    return (
+        pd.DataFrame(
+            {
+                'Latitude' : locations[:,1],
+                'Longitude' : locations[:,2],
+                'Elevation' : locations[:,3],
+                'AverageTemperature' : locations[:,4],
+            }
+        )
+        .join(decode(encoded_trewartha, 'Trewartha'))
+        .join(decode(encoded_climatezone, 'ClimateZone'))
+        .join(classifications)
+    )
 
-def fabricate_dataset(user_id: str, possible_locations: str, train_data_manifest: str) -> None:
+def fabricate_dataset(user_id: str, possible_locations: str, train_data_manifest: str, size: int = 100) -> None:
     """
     Perform dataset creation using possible_locations for identifying
     classes, and write the new dataset to train_data_manifest.
     """
-    df = sample_and_agglomerate_locations(user_id, possible_locations)
+    df = sample_and_agglomerate_locations(user_id, possible_locations, size)
     df.to_csv(train_data_manifest, index=False)
 
 def main(args):
@@ -235,14 +224,16 @@ def main(args):
                 'username' : "x" + str(random.random() * random.random()),
                 'password' : "x" + str(random.random() * random.random())
             }
-        )['data']['location']
+        )['data']['addUser']
+    print(f"User added: {user_id}")
 
     # Set up the classes file
     if args.set_class_data:
         set_class_data(user_id, args.possible_locations)
     
+    print("Beginning final fabrication phase.")
     # Create the master train_data_manifest file
-    fabricate_dataset(user_id, args.possible_locations, args.train_data_manifest)
+    fabricate_dataset(user_id, args.possible_locations, args.train_data_manifest, size=100)
 
     print("Dataset fabricated!")
 
@@ -251,7 +242,7 @@ if __name__ == "__main__":
             prog='dataset.py',
             description='Performs dataset fabrication for the specified csv.'
         )
-    parser.add_argument('--set_class_data', default=True)
+    parser.add_argument('--set_class_data', default=False)
     parser.add_argument('--possible_locations', default='./data/classes.csv')
     parser.add_argument('--train_data_manifest', default='./data/generated/master.csv')
     args = parser.parse_args()
--- location_suggest/model.py
+++ location_suggest/model.py
@@ -9,46 +9,36 @@ import random
 import requests
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 import str2bool
-from dataset import getCoordinates, make_graphql_request, location_json_to_list
-
+from dataset import get_coordinates_by_name, make_graphql_request, location_json_to_list
 
 class DestinationSuggestor:
-    model: Optional[RandomForestClassifier | AdaBoostClassifier]
-    onehot_mapping: np.array
-    country_map: np.array
+    model: Optional[RandomForestClassifier]
 
-    def __init__(self, mapping_filename):
+    def __init__(self, onehot_mapping: List[str]):
         self.model = None
-        self.onehot_mapping = np.append(np.array(["UNK"]), pd.read_csv(mapping_filename)['City'].to_numpy().flatten())
-        self.country_map = np.append(np.array(["UNK"]), pd.read_csv(mapping_filename)['Country'].to_numpy().flatten())
-
+        self.onehot_mapping = onehot_mapping
+    
     def prep_data(self, train_data_manifest_filename: str) -> Tuple[np.ndarray]:
         df = pd.read_csv(train_data_manifest_filename).fillna(0)
-        train_input = []
-        train_labels = []
+        train_input = df.loc[:, df.columns != 'City, Country']
+        train_labels = df['City, Country']
         return (train_input, train_labels)
 
     def train(self, train_input: np.array, train_labels: np.array):
-        self.model = AdaBoostClassifier(random_state=0, n_estimators=100)
+        self.model = RandomForestClassifier()
         self.model.fit(train_input, train_labels)
+
+    def encode_4_destinations(self, destinations: List[List[Any]]) -> np.array:
+        return [location_json_to_list(destination) for destination in destinations]
     
-    def infer(self, past_4_destinations: np.array) -> List[Dict[str, Any]]:
+    def infer(self, past_4_destinations: List[List[Any]]) -> str:
         if self.model:
-            encoded_prediction = self.model.predict(past_4_destinations)
+            encoded_prediction = self.model.predict(self.encode_4_destinations(past_4_destinations))
         else:
             raise RuntimeError("Tried to infer while DestinationSuggestor model was not trained or loaded.")
         
-        decoded_city_names = [self.onehot_mapping[city_num] for city_num in encoded_prediction]
-        decoded_country_names = [self.country_map[country_num] for country_num in encoded_prediction]
-        decoded_city_coordinates = [getCoordinates(city_name) for city_name in decoded_city_names]
-
         # Return a json of the recommended locations
-        return [{
-            "city": city_name,
-            "country": country_name,
-            "latitude": coordinates[0],
-            "longitude": coordinates[1]
-        } for (city_name, country_name, coordinates) in zip(decoded_city_names, decoded_country_names, decoded_city_coordinates)]
+        return self.onehot_mapping[encoded_prediction]
 
     def save(self, filename: str) -> None:
         with open(filename, 'wb') as modelfile:
@@ -62,7 +52,9 @@ class DestinationSuggestor:
 
 
 def main(args):
-    suggestor = DestinationSuggestor()
+    df = pd.read_csv(args.mapping_filename)
+    onehot_mapping = (df['City'] + ", " + df['Country']).to_numpy()
+    suggestor = DestinationSuggestor(onehot_mapping)
 
     if (args.train):
         print("Stage 1: DATA PREPARATION")
@@ -94,10 +86,12 @@ if __name__ == "__main__":
             prog='model.py',
             description='Performs training based on the data in the specified csv.'
         )
-    parser.add_argument('mapping_filename', default='./data/classes.csv')
-    parser.add_argument('train', type=str2bool, default=True)
-    parser.add_argument('train_data_manifest', default='./data/generated/master.csv')
-    parser.add_argument('model_filename', default='./bin/dest_suggestor.pkl')
-    parser.add_argument('user_id', default='653bfedf1e7c5a2367365f16')
+    # Important args
+    parser.add_argument('--mapping_filename', default='./data/classes.csv')
+    parser.add_argument('--train_data_manifest', default='./data/generated/master.csv')
+    parser.add_argument('--train', type=str2bool, default=True)
+    # Optional args
+    parser.add_argument('--model_filename', default='./bin/dest_suggestor.pkl')
+    parser.add_argument('--user_id', default='653bfedf1e7c5a2367365f16')
     args = parser.parse_args()
     main(args)
