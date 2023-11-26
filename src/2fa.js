30b85d9565ec76a442aacb76a86e2b3dd6b7ad28
Milo Gilad <milo@milogilad.com>
2023-11-26 18:16:00 -0500

    Authentication functions

--- src/2fa.js
+++ src/2fa.js
@@ -7,7 +7,7 @@ const rpName = 'TourFusion';
 const rpID = 'tour-fusion.com';
 const origin = `https://${rpID}`;
 
-async function getRegistrationOptions() {
+async function getRegistrationOptions(req, res) {
     // (Pseudocode) Retrieve the user from the database
     // after they've logged in
     const user = getUserFromDB(loggedInUserId); // TODO
@@ -44,7 +44,7 @@ async function getRegistrationOptions() {
     return options;
 }
 
-async function verifyAndSaveRegistration() {
+async function verifyAndSaveRegistration(req, res) {
     const { body } = req;
 
     // (Pseudocode) Retrieve the logged-in user
@@ -89,6 +89,70 @@ async function verifyAndSaveRegistration() {
         // (Pseudocode) Save the authenticator info so that we can
         // get it by user ID later
         saveNewUserAuthenticatorInDB(user, newAuthenticator); // TODO
+    }
+
+    return { verified };
+}
+
+async function getAuthenticationOptions(req, res) {
+    // (Pseudocode) Retrieve the logged-in user
+    const user = getUserFromDB(loggedInUserId); // TODO
+    // (Pseudocode) Retrieve any of the user's previously-
+    // registered authenticators
+    const userAuthenticators  = getUserAuthenticators(user);
+
+    const options = await generateAuthenticationOptions({
+        rpID,
+        // Require users to use a previously-registered authenticator
+        allowCredentials: userAuthenticators.map(authenticator => ({
+            id: authenticator.credentialID,
+            type: 'public-key',
+            transports: authenticator.transports,
+        })),
+        userVerification: 'preferred',
+    });
+
+    // (Pseudocode) Remember this challenge for this user
+    setUserCurrentChallenge(user, options.challenge);
+
+    return options;
+}
+
+async function verifyAuthentication(req, res) {
+    const { body } = req;
+
+    // (Pseudocode) Retrieve the logged-in user
+    const user = getUserFromDB(loggedInUserId);
+    // (Pseudocode) Get `options.challenge` that was saved above
+    const expectedChallenge = getUserCurrentChallenge(user);
+    // (Pseudocode} Retrieve an authenticator from the DB that
+    // should match the `id` in the returned credential
+    const authenticator = getUserAuthenticator(user, body.id);
+
+    if (!authenticator) {
+        throw new Error(`Could not find authenticator ${body.id} for user ${user.id}`);
+    }
+
+    let verification;
+    try {
+        verification = await verifyAuthenticationResponse({
+            response: body,
+            expectedChallenge,
+            expectedOrigin: origin,
+            expectedRPID: rpID,
+            authenticator,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send({ error: error.message });
+    }
+
+    const { verified } = verification;
+    if (verified) {
+        const { authenticationInfo } = verification;
+        const { newCounter } = authenticationInfo;
+
+        saveUpdatedAuthenticatorCounter(authenticator, newCounter);
     }
 
     return { verified };
